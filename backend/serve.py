45c921700b81aa92eaa7959a081c709492276028
Piotr Grędowski <piotrgredowski@gmail.com>
2018-11-27 20:18:46 +0100

    Add basic routes for application
    Created three minimal routes:
    - /api/static (POST) for creating task
    - /api/static (GET) for downloading results of task
    - /api/static/check (GET) for checking status of task
    
    Main script file refactored for possible usage with gunicorn.

--- backend/serve.py
+++ backend/serve.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+from flask import Flask
+
+from views import api
+
+
+def register_blueprints(app):
+    app.register_blueprint(api.blueprint, url_prefix="/api/static")
+
+
+def make_app():
+    app = Flask("Asyncpage")
+
+    register_blueprints(app)
+
+    return app
+
+
 def main():
-    print("Hello!")
+    app = make_app()
+    app.run(host="0.0.0.0",
+            port="8191",
+            debug=True)
 
 
 if __name__ == "__main__":
